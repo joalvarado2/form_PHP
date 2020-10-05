123cd14e344c330a8057c9c63f84b24f665fed4e
Jonathan Alvarado <jonathanalva92@gmail.com>
2020-10-05 16:42:46 -0500

    agregando comentarios

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 $errores = "";
 $enviado = "";
 
-if(isset($_POST["submit"])){
+if(isset($_POST["submit"])){                           // isset valor boleano en este caso valida true para proceder con la validación
     $nombre = $_POST["nombre"];
     $correo = $_POST["correo"];
     $mensaje = $_POST["mensaje"];
 
-    if(!empty($nombre)){
-        $nombre = trim($nombre);
-        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    if(!empty($nombre)){                                         // si el campo nombre no esta vacio  
+        $nombre = trim($nombre);                                 // le quitamos espacios
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);   // le quitamos todo tipo de caracteres *-/+etc
     }else {
         $errores .= "por favor ingresa un nombre <br />";
     }
 
     if(!empty($correo)){
-        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);     // 
 
-        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){          // validamos que sea un correo valido
             $errores .= "por favor ingresa un correo valido <br />";
 
         }
@@ -26,14 +26,14 @@ if(isset($_POST["submit"])){
             $errores .= "por favor ingresa un correo <br />" ;   
     }
     if(!empty($mensaje)){
-        $mensaje = htmlspecialchars($mensaje);
-        $mensaje = trim($mensaje);
-        $mensaje = stripslashes($mensaje);
+        $mensaje = htmlspecialchars($mensaje);          // le quitamos la posibilidad de inyectar código a l cliente
+        $mensaje = trim($mensaje);                      // quitamos espacios
+        $mensaje = stripslashes($mensaje);              // quitamos slashes    
     } else {
         $errores .= "por favor ingresa el mensaje <br />";
     }
 
-    if(!$errores){
+    if(!$errores){      // si no hay errores hacer
         $enviar_a = "jonathanalva92@gmail.com";
         $asunto = "Correo enviado desde Mipagina.com";
         $mensaje_preparado = "De: $nombre \n";
